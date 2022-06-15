1ac8274a96525d3cfd415a2773364ae1c248b4e1
ANA_h <1154073432@qq.com>
2022-06-15 18:16:08 +0800

    add inventoryUIs

---
+++ Assets/Scripts/UI/Inventories/InventorySlotUI.cs
@@ -0,0 +1,51 @@
+﻿using FirstARPG.Inventories;
+using UnityEngine;
+
+namespace FirstARPG.UI.Inventories
+{
+    public class InventorySlotUI : MonoBehaviour
+    {
+        [SerializeField] InventoryItemIcon icon = null;
+        
+        int _index;
+        InventoryItem _item;
+        Inventory _inventory;
+        
+
+        public void Setup(Inventory inventory, int index)
+        {
+            this._inventory = inventory;
+            this._index = index;
+            icon.SetItem(inventory.GetItemInSlot(index), inventory.GetNumberInSlot(index));
+        }
+
+        public int MaxAcceptable(InventoryItem item)
+        {
+            if (_inventory.HasSpaceFor(item))
+            {
+                return int.MaxValue;
+            }
+            return 0;
+        }
+
+        public void AddItems(InventoryItem item, int number)
+        {
+            _inventory.AddItemToSlot(_index, item, number);
+        }
+
+        public InventoryItem GetItem()
+        {
+            return _inventory.GetItemInSlot(_index);
+        }
+
+        public int GetNumber()
+        {
+            return _inventory.GetNumberInSlot(_index);
+        }
+
+        public void RemoveItems(int number)
+        {
+            _inventory.RemoveFromSlot(_index, number);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Inventories/InventoryUI.cs
@@ -0,0 +1,37 @@
+﻿using FirstARPG.Inventories;
+using UnityEngine;
+
+namespace FirstARPG.UI.Inventories
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        [SerializeField] InventorySlotUI InventoryItemPrefab = null;
+        
+        Inventory _playerInventory;
+
+        private void Awake() 
+        {
+            _playerInventory = Inventory.GetPlayerInventory();
+            _playerInventory.inventoryUpdated += Redraw;
+        }
+
+        private void Start()
+        {
+            Redraw();
+        }
+
+        private void Redraw()
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            for (int i = 0; i < _playerInventory.GetSize(); i++)
+            {
+                var itemUI = Instantiate(InventoryItemPrefab, transform);
+                itemUI.Setup(_playerInventory, i);
+            }
+        }
+    }
+}
