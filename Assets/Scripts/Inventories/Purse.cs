c31da7793609cae17da0e65cb6ded03df4c036c7
ANA_h <1154073432@qq.com>
2022-06-18 15:45:20 +0800

    add purse

---
+++ Assets/Scripts/Inventories/Purse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FirstARPG.Saving;
+using UnityEngine;
+
+namespace FirstARPG.Inventories
+{
+    public class Purse :MonoBehaviour ,ISaveable
+    {
+        [SerializeField] private float _startingBalance = 400f;
+
+        private float _balance = 0;
+
+        public event Action onChange;
+
+        private void Awake()
+        {
+            _balance = _startingBalance;
+        }
+
+        public float GetBalance()
+        {
+            return _balance;
+        }
+
+        public void UpdateBalance(float amount)
+        {
+            _balance += amount;
+            onChange?.Invoke();
+        }
+
+        public object CaptureState()
+        {
+            return _balance;
+        }
+
+        public void RestoreState(object state)
+        {
+            _balance = (float) state;
+        }
+    }
+}
