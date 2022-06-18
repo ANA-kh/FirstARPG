bb6c1db9ae1677a3ca3a646d6f0f8e0d7803ab3e
ANA_h <1154073432@qq.com>
2022-06-18 14:25:58 +0800

    add open hide ui

---
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FirstARPG.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        [Serializable]
+        struct KeyToUI
+        {
+            public KeyCode ToggleKey;
+            public GameObject UIContainer;
+        }
+
+        [SerializeField] private List<KeyToUI> _keyToUis;
+        [SerializeField] KeyCode _closeAll = KeyCode.Escape;
+
+        void Update()
+        {
+            foreach (var keyToUi in _keyToUis)
+            {
+                if (Input.GetKeyDown(keyToUi.ToggleKey))
+                {
+                    Toggle(keyToUi.UIContainer);
+                }
+                else if(Input.GetKeyDown(_closeAll))
+                {
+                    keyToUi.UIContainer.SetActive(false);
+                }
+            }
+        }
+
+        public void Toggle(GameObject uiContainer)
+        {
+            uiContainer.SetActive(!uiContainer.activeSelf);
+        }
+    }
+}
