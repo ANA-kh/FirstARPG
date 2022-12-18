94ec265ccf4c798227dc0c625ebd03eb550020d4
ANA_h <1154073432@qq.com>
2022-12-18 16:37:18 +0800

    add Game Base

---
+++ Assets/ScriptsUseXML/GameManager.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using XMLib;
+using XMLib.AM;
+
+namespace XMLibGame
+{
+    public enum InputEvents
+    {
+        None = 0b0000,
+        Moving = 0b0001,
+        Attack = 0b0010,
+        Jump = 0b0100,
+        Jumping = 0b1000,
+    }
+
+    public static class InputData
+    {
+        public static InputEvents InputEvents { get; set; } = InputEvents.None;
+        public static Vector2 AxisValue { get; set; } = Vector2.zero;
+
+        public static bool HasEvent(InputEvents e, bool fullMatch = false)
+        {
+            return fullMatch ? ((InputEvents & e) == InputEvents) : ((InputEvents & e) != 0);
+        }
+
+        public static void Clear()
+        {
+            InputEvents = InputEvents.None;
+            AxisValue = Vector2.zero;
+        }
+    }
+
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        protected List<ActionMachineController> controllers;
+
+        [SerializeField]
+        protected CinemachineBrain _brain;
+
+        [SerializeField]
+        protected List<TextAsset> configs;
+
+        protected float logicTimer = 0f;
+        protected const float logicDeltaTime = 1 / 60f;
+
+        #region Input
+
+        public Controls Input;
+
+        #endregion Input
+
+        private void Awake()
+        {
+            //初始化配置文件加载函数
+            ActionMachineHelper.Init(OnActionMachineConfigLoader);
+            Input = new Controls();
+            Input.Enable();
+
+            Physics.autoSimulation = false;
+        }
+
+        private MachineConfig OnActionMachineConfigLoader(string configName)
+        {
+            TextAsset asset = configs.Find(t => string.Compare(t.name, configName) == 0);
+            return DataUtility.FromJson<MachineConfig>(asset.text);
+        }
+
+        private void OnDestroy()
+        {
+            Input.Disable();
+        }
+
+        private void Update()
+        {
+            UpdateInput();
+            LogicUpdate();
+        }
+
+        private void UpdateInput()
+        {
+            var player = Input.Player;
+            var move = player.Move.ReadValue<Vector2>();
+            if (player.Move.phase == InputActionPhase.Started)
+            {
+                InputData.InputEvents |= InputEvents.Moving;
+                InputData.AxisValue = move;
+            }
+
+            if (player.Attack.triggered)
+            {
+                InputData.InputEvents |= InputEvents.Attack;
+            }
+
+            if (player.Jump.triggered)
+            {
+                InputData.InputEvents |= InputEvents.Jump;
+            }
+
+            if (player.Jump.phase == InputActionPhase.Started)
+            {
+                InputData.InputEvents |= InputEvents.Jumping;
+            }
+        }
+
+        private void LogicUpdate()
+        {
+            logicTimer += Time.deltaTime;
+            if (logicTimer >= logicDeltaTime)
+            {
+                logicTimer -= logicDeltaTime;
+            
+                RunLogicUpdate(logicDeltaTime);
+                _brain.ManualUpdate();//手动更新相机，解决相机抖动的问题
+            }
+            
+            // RunLogicUpdate(Time.deltaTime);
+            // //_brain.ManualUpdate();//手动更新相机，解决相机抖动的问题
+        }
+
+        private void RunLogicUpdate(float logicDeltaTime)
+        {
+            foreach (var item in controllers)
+            {
+                item.LogicUpdate(logicDeltaTime);
+            }
+
+            //更新物理
+            Physics.Simulate(logicDeltaTime);
+
+            //清理输入
+            InputData.Clear();
+        }
+    }
+}
